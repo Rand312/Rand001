430085e70d992176dd8247f58984246591dd3cbb
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-06 20:26:51 +0800

    >  compile NEMU 3018205467 rand Linux rand-virtual-machine 5.4.0-48-generic #52-Ubuntu SMP Thu Sep 10 10:58:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  20:26:51 up  4:11,  1 user,  load average: 0.02, 0.06, 0.07 7497fe9966ae51821c6daabf388785427e57d4e3

--- nemu/src/monitor/debug/watchpoint.c
+++ nemu/src/monitor/debug/watchpoint.c
@@ -38,7 +38,7 @@ WP *new_wp(char *str, int value)
 	new->val=value;   
 	printf("  333  \n");
 			            //assignment
-	strcpy(new->expr,str);
+	strncpy(new->expr,str,32);
 	printf("  444  \n");
 	new->isused=1; 
 
