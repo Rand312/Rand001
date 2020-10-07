16b1850a712668177160820c433068e63a59a805
3018205467-Peng Zejie <766606755@qq.com>
2020-10-07 21:43:59 +0800

    finished the necessary work

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -263,10 +263,6 @@ uint32_t expr(char *e, bool *success)
 	}
 
 	/* TODO: Insert codes to evaluate the expression. */
-	for(int i=0;i<nr_token;i++)
-	{
-		printf(" 333 %d\n",tokens[i].type);
-	}
 	*success=true;
 	return eval(0,nr_token-1);
 }
