aba6f8a4aabbfc89c102a36dec87b88fcd55921f
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-08 13:03:57 +0800

    >  compile NEMU 3018205467 rand Linux rand-virtual-machine 5.4.0-48-generic #52-Ubuntu SMP Thu Sep 10 10:58:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  13:03:57 up 10:54,  1 user,  load average: 0.23, 0.09, 0.04 e9b99c3b4283564158f7fa2ac00230eec6c8e4fa

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -308,6 +308,7 @@ uint32_t expr(char *e, bool *success)   //general evaluate  expression function 
 	{
 		if(tokens[i].type==SUB && (i==0 || tokens[i-1].type==LBRA || tokens[i-1].type==ADD || tokens[i-1].type==SUB || tokens[i-1].type==MUL || tokens[i-1].type==DIV || tokens[i-1].type==MOD))
 		{
+			printf("88888888888888888888888888888888");
 			tokens[i].type=NEG;
 			tokens[i].priority=6;
 		}
