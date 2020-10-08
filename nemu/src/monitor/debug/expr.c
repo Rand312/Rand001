d92fd5840e9f15a0aa90729e5c04a3b94c12062d
tracer-NEMU2020 <tjuics2020@163.com>
2020-10-08 19:40:43 +0800

    >  compile NEMU 3018205467 rand Linux rand-virtual-machine 5.4.0-48-generic #52-Ubuntu SMP Thu Sep 10 10:58:49 UTC 2020 x86_64 x86_64 x86_64 GNU/Linux  19:40:43 up 12:44,  1 user,  load average: 0.26, 0.18, 0.10 b70bd1b91346877cf009a8691cfc820532245f1a

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -292,7 +292,6 @@ uint32_t expr(char *e, bool *success)   //general evaluate  expression function 
 		{
 			tokens[i].type=NEG;
 			tokens[i].priority=6;
-			printf("token[%d].type has changed to the %d",i,NEG);
 		}
 
 		else if(tokens[i].type==MUL && (i==0 || tokens[i-1].type==LBRA || tokens[i-1].type==ADD || tokens[i-1].type==SUB || tokens[i-1].type==MUL || tokens[i-1].type==DIV || tokens[i-1].type==MOD))
